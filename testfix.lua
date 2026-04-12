6320629f2ad4e573cb0078581e6fad1ae9893b41
jdlsc01 <joaotcaudio@gmail.com>
2026-04-11 22:44:27 -0300

    Update7 testfix.lua

--- testfix.lua
+++ testfix.lua
@@ -266,7 +266,7 @@ function Kavo.CreateLib(kavName, themeList)
     Main.ClipsDescendants = true
     Main.AnchorPoint = Vector2.new(0.5, 0.5)
     -- Main.Position = UDim2.new(0.5, 0, 0.5, 0)
-    Main.Position = UDim2.new(0.5, 0, 0.4, 0) 
+    Main.Position = UDim2.new(0.5, 0, 0.45, 0)
     Main.Size = UDim2.new(0, 525, 0, 260)
     -- MainCorner.CornerRadius = UDim.new(0, 4)
     MainCorner.CornerRadius = UDim.new(0, 5)    
